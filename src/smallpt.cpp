3e339ab6393f676d117dd3c0bd4e1600bde8e688
Pedro André Oliveira <ei06125@gcloud.fe.up.pt>
2022-01-13 22:29:36 +0000

    Adds clang-format and applies formatting

--- src/smallpt.cpp
+++ src/smallpt.cpp
@@ -6,7 +6,7 @@
 #include <random>
 double erand48(unsigned short xsubi[3]) {
   std::random_device
-      rd; // Will be used to obtain a seed for the random number engine
+    rd; // Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
   std::uniform_real_distribution<> dis(0.0);
   return dis(gen);
@@ -22,22 +22,24 @@ struct Vec {      // Usage: time ./smallpt 5000 && xv image.ppm
     y = y_;
     z = z_;
   }
-  Vec operator+(const Vec &b) const { return Vec(x + b.x, y + b.y, z + b.z); }
-  Vec operator-(const Vec &b) const { return Vec(x - b.x, y - b.y, z - b.z); }
+  Vec operator+(const Vec& b) const { return Vec(x + b.x, y + b.y, z + b.z); }
+  Vec operator-(const Vec& b) const { return Vec(x - b.x, y - b.y, z - b.z); }
   Vec operator*(double b) const { return Vec(x * b, y * b, z * b); }
-  Vec mult(const Vec &b) const { return Vec(x * b.x, y * b.y, z * b.z); }
-  Vec &norm() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); }
-  double dot(const Vec &b) const {
+  Vec mult(const Vec& b) const { return Vec(x * b.x, y * b.y, z * b.z); }
+  Vec& norm() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); }
+  double dot(const Vec& b) const {
     return x * b.x + y * b.y + z * b.z;
   } // cross:
-  Vec operator%(Vec &b) {
+  Vec operator%(Vec& b) {
     return Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
   }
 };
 
 struct Ray {
   Vec o, d;
-  Ray(Vec o_, Vec d_) : o(o_), d(d_) {}
+  Ray(Vec o_, Vec d_)
+    : o(o_)
+    , d(d_) {}
 };
 
 enum Refl_t { DIFF, SPEC, REFR }; // material types, used in radiance()
@@ -47,8 +49,12 @@ struct Sphere {
   Vec p, e, c; // position, emission, color
   Refl_t refl; // reflection type (DIFFuse, SPECular, REFRactive)
   Sphere(double rad_, Vec p_, Vec e_, Vec c_, Refl_t refl_)
-      : rad(rad_), p(p_), e(e_), c(c_), refl(refl_) {}
-  double intersect(const Ray &r) const { // returns distance, 0 if nohit
+    : rad(rad_)
+    , p(p_)
+    , e(e_)
+    , c(c_)
+    , refl(refl_) {}
+  double intersect(const Ray& r) const { // returns distance, 0 if nohit
     Vec op = p - r.o; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
     double t, eps = 1e-4, b = op.dot(r.d), det = b * b - op.dot(op) + rad * rad;
     if (det < 0)
@@ -60,27 +66,43 @@ struct Sphere {
 };
 
 Sphere spheres[] = {
-    // Scene: radius, position, emission, color, material
-    Sphere(1e5, Vec(1e5 + 1, 40.8, 81.6), Vec(), Vec(.75, .25, .25),
-           DIFF), // Left
-    Sphere(1e5, Vec(-1e5 + 99, 40.8, 81.6), Vec(), Vec(.25, .25, .75),
-           DIFF),                                                     // Rght
-    Sphere(1e5, Vec(50, 40.8, 1e5), Vec(), Vec(.75, .75, .75), DIFF), // Back
-    Sphere(1e5, Vec(50, 40.8, -1e5 + 170), Vec(), Vec(), DIFF),       // Frnt
-    Sphere(1e5, Vec(50, 1e5, 81.6), Vec(), Vec(.75, .75, .75), DIFF), // Botm
-    Sphere(1e5, Vec(50, -1e5 + 81.6, 81.6), Vec(), Vec(.75, .75, .75),
-           DIFF),                                                      // Top
-    Sphere(16.5, Vec(27, 16.5, 47), Vec(), Vec(1, 1, 1) * .999, SPEC), // Mirr
-    Sphere(16.5, Vec(73, 16.5, 78), Vec(), Vec(1, 1, 1) * .999, REFR), // Glas
-    Sphere(600, Vec(50, 681.6 - .27, 81.6), Vec(12, 12, 12), Vec(),
-           DIFF) // Lite
+  // Scene: radius, position, emission, color, material
+  Sphere(1e5,
+         Vec(1e5 + 1, 40.8, 81.6),
+         Vec(),
+         Vec(.75, .25, .25),
+         DIFF), // Left
+  Sphere(1e5,
+         Vec(-1e5 + 99, 40.8, 81.6),
+         Vec(),
+         Vec(.25, .25, .75),
+         DIFF),                                                     // Rght
+  Sphere(1e5, Vec(50, 40.8, 1e5), Vec(), Vec(.75, .75, .75), DIFF), // Back
+  Sphere(1e5, Vec(50, 40.8, -1e5 + 170), Vec(), Vec(), DIFF),       // Frnt
+  Sphere(1e5, Vec(50, 1e5, 81.6), Vec(), Vec(.75, .75, .75), DIFF), // Botm
+  Sphere(1e5,
+         Vec(50, -1e5 + 81.6, 81.6),
+         Vec(),
+         Vec(.75, .75, .75),
+         DIFF),                                                      // Top
+  Sphere(16.5, Vec(27, 16.5, 47), Vec(), Vec(1, 1, 1) * .999, SPEC), // Mirr
+  Sphere(16.5, Vec(73, 16.5, 78), Vec(), Vec(1, 1, 1) * .999, REFR), // Glas
+  Sphere(600,
+         Vec(50, 681.6 - .27, 81.6),
+         Vec(12, 12, 12),
+         Vec(),
+         DIFF) // Lite
 };
 
-inline double clamp(double x) { return x < 0 ? 0 : x > 1 ? 1 : x; }
+inline double clamp(double x) {
+  return x < 0 ? 0 : x > 1 ? 1 : x;
+}
 
-inline int toInt(double x) { return int(pow(clamp(x), 1 / 2.2) * 255 + .5); }
+inline int toInt(double x) {
+  return int(pow(clamp(x), 1 / 2.2) * 255 + .5);
+}
 
-inline bool intersect(const Ray &r, double &t, int &id) {
+inline bool intersect(const Ray& r, double& t, int& id) {
   double n = sizeof(spheres) / sizeof(Sphere), d, inf = t = 1e20;
   for (int i = int(n); i--;)
     if ((d = spheres[i].intersect(r)) && d < t) {
@@ -90,12 +112,12 @@ inline bool intersect(const Ray &r, double &t, int &id) {
   return t < inf;
 }
 
-Vec radiance(const Ray &r, int depth, unsigned short *Xi) {
+Vec radiance(const Ray& r, int depth, unsigned short* Xi) {
   double t;   // distance to intersection
   int id = 0; // id of intersected object
   if (!intersect(r, t, id))
     return Vec();                  // if miss, return black
-  const Sphere &obj = spheres[id]; // the hit object
+  const Sphere& obj = spheres[id]; // the hit object
   Vec x = r.o + r.d * t, n = (x - obj.p).norm(),
       nl = n.dot(r.d) < 0 ? n : n * -1, f = obj.c;
   double p = f.x > f.y && f.x > f.z ? f.x : f.y > f.z ? f.y : f.z; // max refl
@@ -121,30 +143,29 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi) {
       0) // Total internal reflection
     return obj.e + f.mult(radiance(reflRay, depth, Xi));
   Vec tdir =
-      (r.d * nnt - n * ((into ? 1 : -1) * (ddn * nnt + sqrt(cos2t)))).norm();
+    (r.d * nnt - n * ((into ? 1 : -1) * (ddn * nnt + sqrt(cos2t)))).norm();
   double a = nt - nc, b = nt + nc, R0 = a * a / (b * b),
          c = 1 - (into ? -ddn : tdir.dot(n));
   double Re = R0 + (1 - R0) * c * c * c * c * c, Tr = 1 - Re, P = .25 + .5 * Re,
          RP = Re / P, TP = Tr / (1 - P);
-  return obj.e +
-         f.mult(depth > 2
-                    ? (erand48(Xi) < P ? // Russian roulette
-                           radiance(reflRay, depth, Xi) * RP
+  return obj.e + f.mult(depth > 2 ? (erand48(Xi) < P
+                                       ? // Russian roulette
+                                       radiance(reflRay, depth, Xi) * RP
                                        : radiance(Ray(x, tdir), depth, Xi) * TP)
-                    : radiance(reflRay, depth, Xi) * Re +
-                          radiance(Ray(x, tdir), depth, Xi) * Tr);
+                                  : radiance(reflRay, depth, Xi) * Re +
+                                      radiance(Ray(x, tdir), depth, Xi) * Tr);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   int w = 1024, h = 768, samps = argc == 2 ? atoi(argv[1]) / 4 : 1; // # samples
   Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm()); // cam pos, dir
   Vec cx = Vec(w * .5135 / h), cy = (cx % cam.d).norm() * .5135, r,
       *c = new Vec[w * h];
 #pragma omp parallel for schedule(dynamic, 1) private(r) // OpenMP
   for (int y = 0; y < h; y++) {                          // Loop over image rows
-    fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samps * 4,
-            100. * y / (h - 1));
-    for (unsigned short x = 0, Xi[3] = {0, 0, y * y * y}; x < w;
+    fprintf(
+      stderr, "\rRendering (%d spp) %5.2f%%", samps * 4, 100. * y / (h - 1));
+    for (unsigned short x = 0, Xi[3] = { 0, 0, y * y * y }; x < w;
          x++) // Loop cols
       for (int sy = 0, i = (h - y - 1) * w + x; sy < 2;
            sy++)                                    // 2x2 subpixel rows
@@ -162,7 +183,7 @@ int main(int argc, char *argv[]) {
           c[i] = c[i] + Vec(clamp(r.x), clamp(r.y), clamp(r.z)) * .25;
         }
   }
-  FILE *f = fopen("image.ppm", "w"); // Write image to PPM file.
+  FILE* f = fopen("image.ppm", "w"); // Write image to PPM file.
   fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
   for (int i = 0; i < w * h; i++)
     fprintf(f, "%d %d %d ", toInt(c[i].x), toInt(c[i].y), toInt(c[i].z));
